374348fe025fa79ba6ced757100d8eb3303a6187
smvd <semvd06@gmail.com>
2021-03-26 10:17:18 +0100

    '-'

--- C/Cclips-Terminal (V 2.0)/Cclips-Terminal.c
+++ C/Cclips-Terminal (V 2.0)/Cclips-Terminal.c
@@ -39,10 +39,58 @@ Enable ANSI - reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_D
 
 #include <stdio.h>																// Lib for IO
 #include <string.h>																// Lib for string functions
-#include "CustomMath.c"															// Import the file holding the math functions
+#include <stdlib.h>																// The standard library
 
 #define TRUE 	1 																// Bool true
 #define FALSE 	0 																// Bool false
+
+float CustomMath(char num1[100], char action[3], char num2[100])
+{
+	float buff = 0.0;
+	float x = atof(num1);
+	float y = atof(num2);
+	printf("%f %f\n",x,y);
+
+	if (strcmp(action, "**") == 0)
+	{
+		if (y == 1) {return x;}
+		else if (y == 0 && x != 0) {return 1;}
+		else if (y == 0 && x == 0) {return 0;}
+		else
+		{
+			buff = x * x;
+			for (int i = 2; i < y; i++)
+			{
+				buff *= x;
+			}
+			return buff;
+		}
+	}
+	else if (strcmp(action, "+") == 0)
+	{
+		return x+y;
+	}
+	else if (strcmp(action, "-") == 0)
+	{
+		return x-y;
+	}
+	else if (strcmp(action, "*") == 0)
+	{
+		return x*y;
+	}
+	else if (strcmp(action, "/") == 0)
+	{
+		return x/y;
+	}
+	else if (strcmp(action, "%") == 0)
+	{
+		buff = x/y;
+		int temp = buff;
+		buff = temp*y;
+		return x-buff;
+	}
+}
+
 
 void error(char type[100], char message[100])									// Function to throw custom error
 {
@@ -62,13 +110,12 @@ int main()																		// Main entry point
 	
 	char in[100];																// Char array for input
 	char buff[100];																// A place to store temps values
-	char parsed[10][100];														// String array to hold the parsed input
+	char parsed[30][100];														// String array to hold the parsed input
 
 	while (TRUE)																// Inf loop
 	{
 		for (int i = 0; i < 10; i++)											// Loop 10 times
 		{
-			if (parsed[i][0] == '\0') {break;}									// If its the end of the array, stop
 			strcpy(parsed[i], "");												// Set the current slot to blank
 		}
 
@@ -77,50 +124,49 @@ int main()																		// Main entry point
 		fgets(in, 100, stdin);													// Get the input
 		strtok(in, "\n");														// Remove the enter character
 
-		if (strchr("0123456789", in[0]))										// If its a number
+		if (strchr("0123456789(", in[0]))										// If its a number
 		{
 			/* - MATH LEXER - */
 			int x = 0;															// Var to hold the current slot we are writing
+			int par = 0;
 			for (unsigned int i = 0; i < strlen(in); i++)						// Loop threw each character in the input
 			{
-				if (strchr("0123456789.", in[i]))								// If its a number or a .
+				if (in[i] == '(')
 				{
-					append(parsed[x], in[i]);									// Append the character to the current slot
-					x++;														// Activate the next slot
-					if (strchr("0123456789", in[i+1])) 							// If the next character is a number
-					{
-						x--;													// Activat the last slot
-					}
+					if (par != 0) {append(parsed[x], in[i]);} 
+					par++;
 				}
-				else if (in[i] == ')' || in[i] == '(')							// If its parentheses
+				else if (in[i] == ')')
 				{
-					append(parsed[x], in[i]);									// Append the character to the current slot
-					x++;														// Activate the next slot
+					par--;
+					if (par == 0) {x++;}
+					if (par != 0) {append(parsed[x], in[i]);}
 				}
-				else if (strchr("+-*/%", in[i]))								// If its a math operator
+				else if (par == 0 && strchr("0123456789.",in[i]))
 				{
-					append(parsed[x], in[i]);									// Append the character to the current slot
-					x++;														// Activate the next slot
-					if (in[i+1] == '*')				 							// If the next character is *
-					{
-						x--;													// Activat the last slot
-					}
+					append(parsed[x], in[i]);
+					if (!strchr("0123456789", in[i+1])){x++;}
 				}
-				else															// If it isnt one of the above
+				else if (par == 0 && strchr("+-/*%",in[i]))
 				{
-					if (in[i] == ' ')											// If its a space 
-					{
-						continue;												// Ignore it
-					}
-					snprintf(buff, 100, "Invalid character at index: %d", i); 	// Put error message into buffer
-					error("SYNTAX", buff);										// Throw error
-					goto top;													// End operation
-				}				
+					append(parsed[x], in[i]);
+					if (in[i+1] != '*'){x++;}
+				}
+				else if (!par == 0)
+				{
+					append(parsed[x], in[i]);
+				}
+				else
+				{
+					sprintf(buff, "Invalid character at index: %d", i);
+					error("SYNTAX", buff);
+					goto top;
+				}	
 			}
 
 			/* - FORMAT THE PARSED OUTPUT - */
 			printf("[ %s", parsed[0]);											// Print some stuff
-			for (int i = 1; i < 10; i++)										// Loop 10 times
+			for (int i = 1; i < 30; i++)										// Loop 30 times
 			{
 				if (parsed[i][0] == '\0') {break;}								// If its the end of the array, stop
 				printf(" : %s", parsed[i]);										// Print some stuff
